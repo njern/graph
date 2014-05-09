57f1a3075dd1d976bf2716725127b2af37ba9565
Niclas Jern <niclas@walkbase.com>
2014-05-09 11:44:20 +0300

    Support the alternative format used for the fourth exercise benchmark file. Add functions for determining minimum edge/vertex coloring. Add a few support functions used by edge/vertex coloring functions.

--- undirected_graph.go
+++ undirected_graph.go
@@ -11,6 +11,7 @@ import (
 
 type UndirectedGraph struct {
 	edges    map[Vertex][]Edge // [Start]Edges
+	edgeList []Edge
 	vertices []Vertex
 }
 
@@ -31,11 +32,47 @@ func (g *UndirectedGraph) String() string {
 	return s
 }
 
+// vertexNeighbours returns a list of v's
+// neighbour vertices to which it is directly
+// connected with an edge.
+func (g *UndirectedGraph) vertexNeighbours(v Vertex) Vertices {
+	var vertices Vertices
+	for _, edge := range g.edges[v] {
+		vertices = append(vertices, edge.end)
+	}
+
+	return vertices
+}
+
+// edgeNeighbours returns all the edges neighbouring the given edge.
+// It will only return one "version" of each Edge, not both the
+// forwards and backwards version.
+func (g *UndirectedGraph) edgeNeighbours(e Edge) []Edge {
+	edges := make(map[Edge]bool)
+
+	for _, edge := range g.edgeList {
+		if e.start == edge.start || e.start == edge.end ||
+			e.end == edge.start || e.end == edge.end {
+			edges[edge] = true
+
+		}
+	}
+
+	var result []Edge
+	for edge := range edges {
+		result = append(result, edge)
+	}
+
+	return result
+}
+
 func (g *UndirectedGraph) AddEdge(e Edge) {
 	if g.edges == nil {
 		// Lazily initialize
 		g.edges = make(map[Vertex][]Edge)
 	}
+
+	g.edgeList = append(g.edgeList, e)
 
 	reverseEdge := Edge{start: e.end, end: e.start, weight: e.weight, id: e.id}
 	edgeFound := false
@@ -119,8 +156,21 @@ func NewUndirectedGraphFromFile(filePath string, valueSeparator rune) (*Undirect
 			return nil, err
 		}
 
-		// We're expecting four values
-		if len(record) != 4 {
+		// Some graphs leave out the weight
+		if len(record) == 3 {
+			// Dig out the values, skip the row if invalid values
+			start := Vertex{id: record[0]}
+			end := Vertex{id: record[1]}
+			id := record[2]
+			// Create & add Edge
+			e := Edge{start: start, end: end, weight: -1, id: id}
+			g.AddEdge(e)
+
+			continue
+		}
+
+		// We're expecting three or four values
+		if len(record) != 3 && len(record) != 4 {
 			continue
 		}
 
@@ -180,4 +230,117 @@ func (g *UndirectedGraph) PrimMST(start Vertex) []Edge {
 	}
 
 	return eNew
+}
+
+// VertexColors iteratively applies a greedy algorithm of
+// "smallest possible color" to find the minimum
+// vertex coloring for a given undirected graph.
+// It will loop over the list of vertices and set the color
+// to the smallest integer not used by one of the vertex's
+// neighbours until no more optimisations can be made.
+func (g *UndirectedGraph) VertexColors() map[Vertex]int {
+	// Track vertex colors & start off at math.Maxint32
+	vertexColors := make(map[Vertex]int)
+	for _, v := range g.vertices {
+		vertexColors[v] = math.MaxInt32
+	}
+
+	for {
+		graphChangedDuringCurrentPass := false
+		for vertex := range vertexColors {
+			// Get list of neighbour vertices
+			neighbours := g.vertexNeighbours(vertex)
+
+			// Find the smallest color(integer)
+			// not already assigned to one of
+			// the vertex's neighbours.
+			color := 0
+			for {
+				shouldContinue := false
+				for _, neighbour := range neighbours {
+					if vertexColors[neighbour] == color {
+						// If one of the neighbours are already
+						// painted with this color, continue
+						// on to the next.
+						shouldContinue = true
+					}
+				}
+				if shouldContinue == false {
+					break
+				}
+				// Else, we try with the next color.
+				color++
+			}
+			// If the old color was larger than the new
+			// color, replace it.
+			if vertexColors[vertex] > color {
+				graphChangedDuringCurrentPass = true
+				vertexColors[vertex] = color
+			}
+		}
+
+		// Once we can no longer improve the graph...
+		if graphChangedDuringCurrentPass == false {
+			break
+		}
+	}
+
+	return vertexColors
+}
+
+// EdgeColors iteratively applies a greedy algorithm of
+// "smallest possible color" to find the minimum edge
+// coloring for a given undirected graph in a way very
+// similar to VertexColors().
+// It will loop over the list of edges and set the color
+// to the smallest integer not used by one of the edge's
+// neighbours until no more optimisations can be made.
+func (g *UndirectedGraph) EdgeColors() map[Edge]int {
+	// Track edge colors & start off at math.Maxint32
+	edgeColors := make(map[Edge]int)
+	for _, e := range g.edgeList {
+		edgeColors[e] = math.MaxInt32
+	}
+
+	for {
+		graphChangedDuringCurrentPass := false
+		for edge := range edgeColors {
+			// Get list of neighbour vertices
+			neighbours := g.edgeNeighbours(edge)
+
+			// Find the smallest color(integer)
+			// not already assigned to one of
+			// the edge's neighbours.
+			color := 0
+			for {
+				shouldContinue := false
+				for _, neighbour := range neighbours {
+					if edgeColors[neighbour] == color {
+						// If one of the neighbours are already
+						// painted with this color, continue
+						// on to the next.
+						shouldContinue = true
+					}
+				}
+				if shouldContinue == false {
+					break
+				}
+				// Else, we try with the next color.
+				color++
+			}
+			// If the old color was larger than the new
+			// color, replace it.
+			if edgeColors[edge] > color {
+				graphChangedDuringCurrentPass = true
+				edgeColors[edge] = color
+			}
+		}
+
+		// Once we can no longer improve the graph...
+		if graphChangedDuringCurrentPass == false {
+			break
+		}
+	}
+
+	return edgeColors
 }
